046e431755e997a0df07cab5fd189c24c2066a6c
Andrey <avtogol1998@gmail.com>
2020-12-13 12:18:55 +0200

    Added lib install

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from setuptools import setup, find_packages
+from pathlib import Path
+from typing import List
+from setuptools import setup
+
+
+def parse_requirements() -> List[str]:
+    path = Path() / 'requirements.txt'
+    with open(str(path), 'r') as file:
+        data = file.read()
+    lines = data.split('\n')
+    return [line.strip() for line in lines if line.strip()]
+
 
 setup(name='ecg_lib',
       version='0.1',
@@ -7,5 +18,5 @@ setup(name='ecg_lib',
       author='Andrii Tereshchenko',
       author_email='avtogol1998@gmail.com',
       description='Library for build word2vec model based on ECG signal',
-      packages=find_packages(),
+      install_requires=parse_requirements(),
       zip_safe=False)
