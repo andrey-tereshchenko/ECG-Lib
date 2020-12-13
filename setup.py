ce8af650dd2d0684d3089475d0df1008072736ef
Andrey <avtogol1998@gmail.com>
2020-12-13 12:16:15 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='ecg_lib',
+      version='0.1',
+      url='https://github.com/andrey-tereshchenko/ECG-Lib',
+      license='MIT',
+      author='Andrii Tereshchenko',
+      author_email='avtogol1998@gmail.com',
+      description='Library for build word2vec model based on ECG signal',
+      packages=find_packages(),
+      zip_safe=False)
